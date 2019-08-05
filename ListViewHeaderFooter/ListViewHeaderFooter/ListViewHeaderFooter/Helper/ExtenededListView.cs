ac970f27a8c30f3905a958b30eaf778ae9af5147
Gnana Priya N <gnanapriya.namasivayam@syncfusion.com>
2019-08-05 07:05:41 +0530

    updated sample and read me file

---
+++ ListViewHeaderFooter/ListViewHeaderFooter/ListViewHeaderFooter/Helper/ExtenededListView.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AutoFitSample
+{
+    public class SfListViewExt : SfListView
+    {
+        Grid headerGrid;
+        Grid footerGrid;
+
+        public SfListViewExt()
+        {
+            headerGrid = new Grid();
+            headerGrid.BackgroundColor = Color.Teal;
+            Label headerLabel = new Label();
+            headerLabel.Text = "Header Item";
+            headerLabel.FontSize = 18;
+            headerLabel.TextColor = Color.White;
+            headerLabel.HorizontalOptions = LayoutOptions.Center;
+            headerLabel.VerticalOptions = LayoutOptions.Center;
+            headerGrid.Children.Add(headerLabel);
+
+            footerGrid = new Grid();
+            footerGrid.BackgroundColor = Color.Teal;
+            Label footerLabel = new Label();
+            footerLabel.Text = "Footer Item";
+            footerLabel.FontSize = 18;
+            footerLabel.TextColor = Color.White;
+            footerLabel.HorizontalOptions = LayoutOptions.Center;
+            footerLabel.VerticalOptions = LayoutOptions.Center;
+            footerGrid.Children.Add(footerLabel);
+
+            this.Children.Add(headerGrid);
+            this.Children.Add(footerGrid);
+        }
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            headerGrid.Layout(new Rectangle(0, 0, width, 70));
+            footerGrid.Layout(new Rectangle(0, height - 70, width, 70));
+            base.LayoutChildren(0, 70, width, height);
+        }
+    }
+}
---
+++ ListViewHeaderFooter/ListViewHeaderFooter/ListViewHeaderFooter/ViewModel/ViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Syncfusion.ListView.XForms;
+
+namespace AutoFitSample 
+{
+    public class ListViewAutoFitContentViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private ObservableCollection<ListViewBookInfo> bookInfo;
+        private Command<object> tappedCommand; 
+
+        #endregion
+
+        #region Constructor
+
+        public ListViewAutoFitContentViewModel()
+        {
+            tappedCommand = new Command<object>(OnTapped);
+            GenerateSource();
+        }
+
+        private void OnTapped(object obj)
+        {
+            Application.Current.MainPage.DisplayAlert("Tapped Command", "Header/Footer item is tapped", "Close");
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Command<object> TappedCommand
+        {
+            get { return tappedCommand; }
+            set
+            {
+                tappedCommand = value;
+                OnPropertyChanged("TappedCommand");
+            }
+        }
+        public ObservableCollection<ListViewBookInfo> BookInfo
+        {
+            get { return bookInfo; }
+            set { this.bookInfo = value; }
+        }
+
+        #endregion
+
+        #region Generate Source
+
+        private void GenerateSource()
+        {
+            ListViewBookInfoRepository bookInfoRepository = new ListViewBookInfoRepository();
+            bookInfo = bookInfoRepository.GetBookInfo();
+        }
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
